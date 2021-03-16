33b999b9ce9d478008605aca824fc438713cd314
Matthew Machurek <mrjm2005@yahoo.com>
2021-03-16 13:10:31 -0500

    destroy is working

--- watchparty/watchpartyapi/views/watchparty.py
+++ watchparty/watchpartyapi/views/watchparty.py
@@ -78,6 +78,23 @@ class WatchParties(ViewSet):
 
         return Response({}, status=status.HTTP_204_NO_CONTENT)
 
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single watch party
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            watchparty = WatchParty.objects.get(pk=pk)
+            watchparty.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Event.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class GameSerializer(serializers.ModelSerializer):
     """JSON serializer for games
