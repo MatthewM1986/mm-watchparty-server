4c2e79d43e9870594dee88f30cbb3bcac9332f98
Matthew Machurek <mrjm2005@yahoo.com>
2021-03-16 11:56:20 -0500

    able to update now

--- watchparty/watchpartyapi/views/watchparty.py
+++ watchparty/watchpartyapi/views/watchparty.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseServerError
 from django.core.exceptions import ValidationError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import serializers
 from watchpartyapi.models import WatchParty, Fan, Game
 from watchpartyapi.views.game import GameSerializer
@@ -61,6 +62,33 @@ class WatchParties(ViewSet):
         except ValidationError as ex:
             return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
 
+    def update(self, request, pk=None):
+        """Handle PUT requests for a watch party
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        watchparty = WatchParty.objects.get(pk=pk)
+        watchparty.name = request.data["name"]
+        watchparty.scheduled_time = request.data["scheduled_time"]
+        watchparty.location = request.data["location"]
+        watchparty.number_of_fans = request.data["number_of_fans"]
+        game = Game.objects.get(pk=request.data["gameId"])
+        watchparty.game = game
+        watchparty.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+
+class GameSerializer(serializers.ModelSerializer):
+    """JSON serializer for games
+    Arguments:
+        serializers
+    """
+    class Meta:
+        model = Game
+        fields = ('sport_type', 'team_one', 'team_two', 'date', 'description')
+        depth = 2
+
 
 class WatchPartySerializer(serializers.ModelSerializer):
     """JSON serializer for watch parties
@@ -73,15 +101,4 @@ class WatchPartySerializer(serializers.ModelSerializer):
         model = WatchParty
         fields = ('name', 'scheduled_time', 'game',
                   'location', 'number_of_fans')
-        depth = 2
-
-
-class GameSerializer(serializers.ModelSerializer):
-    """JSON serializer for games
-    Arguments:
-        serializers
-    """
-    class Meta:
-        model = Game
-        fields = ('sport_type', 'team_one', 'team_two', 'date', 'description')
         depth = 2
